bae522afb00908e6492d56f7af7d6f3cb0ac32f2
Maminirina Andriamasinoro <maminirinaandria17@gmail.com>
2025-02-23 21:33:51 +0300

    update card
    Update des cards (zao izy mandeha fa mbola exemple)

--- card.py
+++ card.py
@@ -1,5 +1,5 @@
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.button import MDRaisedButton, MDRectangleFlatButton
 from kivymd.uix.card import MDCard
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.label import MDLabel
@@ -9,35 +9,64 @@ class contrat(MDCard):
     def __init__(self, client, date_contrat, type_traitement, durée, debut_contrat, fin_prévu, **kwargs):
         super().__init__(**kwargs)
         self.size_hint = (None, None)
-        self.size = ("300dp", "360dp")  # Taille ajustée
-        self.padding = "8dp"
+        self.size = ("300dp", "390dp")  # Taille ajustée
+        self.padding = ("15dp", "15dp", "0dp", "10dp")
         self.elevation = 1
         self.orientation = "vertical"
         self.pos_hint= {"center_x": .5, "center_y": .7}
         self.md_bg_color= '#56B5FB'
+        self.radius = 10
 
         #informations
-        info_conteneur = MDGridLayout(cols=1, padding=("0dp", "1dp", "0dp", "1dp"))
+        info_conteneur = MDGridLayout(cols=1)
 
-        info_conteneur.add_widget(MDLabel(text=f"Client : {client}", font_style="H6", halign="left"))
-        info_conteneur.add_widget(MDLabel(text=f"Date du contrat : {date_contrat}", font_style="Subtitle1", halign="left"))
-        info_conteneur.add_widget(MDLabel(text=f"Traitement(Type) : {type_traitement}", font_style="Subtitle1", halign="left"))
-        info_conteneur.add_widget(MDLabel(text=f"Durée du contrat : {durée}", font_style="Subtitle1", halign="left"))
-        info_conteneur.add_widget(MDLabel(text=f"Début du contrat : {debut_contrat}", font_style="Subtitle1", halign="left"))
-        info_conteneur.add_widget(MDLabel(text=f"Fin prévu du contrat : {fin_prévu}", font_style="Subtitle1", halign="left"))
+        info_conteneur.add_widget(MDLabel(text=f"Client : {client}",
+                                          font_style="Body1",
+                                          halign="left",
+                                          font_name= 'poppins',
+                                          font_size= 12))
+        info_conteneur.add_widget(MDLabel(text=f"Date du contrat : {date_contrat}",
+                                          font_style="Body1",
+                                          halign="left",
+                                          font_name= 'poppins',
+                                          font_size= 12))
+        info_conteneur.add_widget(MDLabel(text=f"Traitement(Type) : {type_traitement}",
+                                          font_style="Body1",
+                                          halign="left",
+                                          font_name= 'poppins',
+                                          font_size= 12))
+        info_conteneur.add_widget(MDLabel(text=f"Durée du contrat : {durée}",
+                                          font_style="Body1",
+                                          halign="left",
+                                          font_name= 'poppins',
+                                          font_size= 12))
+        info_conteneur.add_widget(MDLabel(text=f"Début du contrat : {debut_contrat}",
+                                          font_style="Body1",
+                                          halign="left",
+                                          font_name= 'poppins',
+                                          font_size= 12))
+        info_conteneur.add_widget(MDLabel(text=f"Fin prévu du contrat : {fin_prévu}",
+                                          font_style="Body1",
+                                          halign="left",
+                                          font_name= 'poppins',
+                                          font_size= 12))
 
         #Conteneur principal
-        conteneur_main = MDBoxLayout(orientation="vertical")
+        conteneur_main = MDBoxLayout(orientation="vertical", padding='15dp')
 
         #Bouton
-        conteneur_btn = MDBoxLayout(size_hint_y=None, height="50dp", padding=("10dp", "80dp", "10dp", "30dp"))
-        more_info_btn = MDRaisedButton(
+        conteneur_btn = MDBoxLayout(size_hint_y=.5,
+                                    height="50dp",
+                                    padding=("0dp", "50dp", "20dp", "20dp"))
+        more_info_btn = MDRectangleFlatButton(
             text="Plus d'informations",
-            size_hint=(1, None),
-            height="40dp",
-            font_size= 17,
+            size_hint=(1, 1.3),
+            font_size= 15,
             md_bg_color= 'white',
-            _default_text_color='black'
+            _default_text_color='black',
+            _radius = 8,
+            line_color= 'white',
+            font_name= 'poppins'
         )
         conteneur_btn.add_widget(more_info_btn)
 
