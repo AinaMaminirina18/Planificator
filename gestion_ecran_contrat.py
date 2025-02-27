2cc10497ae0d214a1fc988e3700d1af42bc62ec0
Maminirina Andriamasinoro <maminirinaandria17@gmail.com>
2025-02-27 23:10:56 +0300

    Ajout gestion ecran
    ajout des gestionnaire d'ecran pour chaque page

---
+++ gestion_ecran_contrat.py
@@ -0,0 +1,7 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
+
+def gestion_ecran_contrat(manager):
+    manager.add_widget(Builder.load_file(f'screen/option_contrat.kv'))
+    manager.add_widget(Builder.load_file(f'screen/new-contrat.kv'))
+    manager.add_widget(Builder.load_file(f'screen/suppr_contrat.kv'))
