5f0c38c0f2affefff85d797977cac29bc6a507ef
Maminirina Andriamasinoro <maminirinaandria17@gmail.com>
2025-02-28 21:51:29 +0300

    update

--- gestion_ecran_contrat.py
+++ gestion_ecran_contrat.py
@@ -5,3 +5,4 @@ def gestion_ecran_contrat(manager):
     manager.add_widget(Builder.load_file(f'screen/option_contrat.kv'))
     manager.add_widget(Builder.load_file(f'screen/new-contrat.kv'))
     manager.add_widget(Builder.load_file(f'screen/suppr_contrat.kv'))
+    manager.add_widget(Builder.load_file(f'screen/option_client.kv'))
