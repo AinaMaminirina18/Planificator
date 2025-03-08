c42dda9f2332d53f95256366f5f5e9f0b933c9f9
Maminirina Andriamasinoro <maminirinaandria17@gmail.com>
2025-03-08 19:58:33 +0300

    Ajout des ecrans comptes

---
+++ gestion_ecran_compte.py
@@ -0,0 +1,8 @@
+from kivy.lang.builder import Builder
+
+
+def gestion_ecran_compte(manager):
+    manager.add_widget(Builder.load_file('screen/about_compte.kv'))
+    manager.add_widget(Builder.load_file('screen/suppr_compte.kv'))
+    manager.add_widget(Builder.load_file('screen/compte_not_admin.kv'))
+    manager.add_widget(Builder.load_file('screen/modif_compte.kv'))
