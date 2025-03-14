5b6304ad46751fcb8229afb3b270ae9079f7b9d8
Maminirina Andriamasinoro <maminirinaandria17@gmail.com>
2025-03-14 20:04:02 +0300

    update chemin ecran

--- gestion_ecran_contrat.py
+++ gestion_ecran_contrat.py
@@ -2,10 +2,10 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 
 def gestion_ecran_contrat(manager):
-    manager.add_widget(Builder.load_file(f'screen/option_contrat.kv'))
-    manager.add_widget(Builder.load_file(f'screen/new-contrat.kv'))
-    manager.add_widget(Builder.load_file(f'screen/suppr_contrat.kv'))
-    manager.add_widget(Builder.load_file(f'screen/ajout_info_client.kv'))
-    manager.add_widget(Builder.load_file(f'screen/save_info_client.kv'))
-    manager.add_widget(Builder.load_file(f'screen/ajout_planning_contrat.kv'))
-    manager.add_widget(Builder.load_file(f'screen/facture_contrat.kv'))
+    manager.add_widget(Builder.load_file(f'screen/contrat/option_contrat.kv'))
+    manager.add_widget(Builder.load_file(f'screen/contrat/new-contrat.kv'))
+    manager.add_widget(Builder.load_file(f'screen/contrat/suppr_contrat.kv'))
+    manager.add_widget(Builder.load_file(f'screen/client/ajout_info_client.kv'))
+    manager.add_widget(Builder.load_file(f'screen/client/save_info_client.kv'))
+    manager.add_widget(Builder.load_file(f'screen/contrat/ajout_planning_contrat.kv'))
+    manager.add_widget(Builder.load_file(f'screen/contrat/facture_contrat.kv'))
