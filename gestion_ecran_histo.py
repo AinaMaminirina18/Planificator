4eac80b2af4fd9949df85837322ebde58844c0ff
Maminirina Andriamasinoro <maminirinaandria17@gmail.com>
2025-03-14 20:04:59 +0300

    Creation de fichier historique et deplacement des ecrans

--- gestion_ecran_histo.py
+++ gestion_ecran_histo.py
@@ -2,4 +2,5 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 
 def gestion_ecran_histo(manager):
-    manager.add_widget(Builder.load_file(f'screen/option_histo.kv'))
+    manager.add_widget(Builder.load_file(f'screen/historique/option_histo.kv'))
+    manager.add_widget(Builder.load_file(f'screen/historique/histo_remarque.kv'))
