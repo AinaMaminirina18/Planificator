8146542f6cd53e05feae7fded5d9dec60da7bad3
Maminirina Andriamasinoro <maminirinaandria17@gmail.com>
2025-03-14 20:03:48 +0300

    Creation de fichier compte et deplacement des ecrans

--- gestion_ecran_compte.py
+++ gestion_ecran_compte.py
@@ -2,6 +2,6 @@ from kivy.lang.builder import Builder
 
 
 def gestion_ecran_compte(manager):
-    manager.add_widget(Builder.load_file('screen/about_compte.kv'))
-    manager.add_widget(Builder.load_file('screen/suppr_compte.kv'))
-    manager.add_widget(Builder.load_file('screen/modif_compte.kv'))
+    manager.add_widget(Builder.load_file('screen/compte/about_compte.kv'))
+    manager.add_widget(Builder.load_file('screen/compte/suppr_compte.kv'))
+    manager.add_widget(Builder.load_file('screen/compte/modif_compte.kv'))
