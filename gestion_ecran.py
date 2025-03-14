5d4ddde28c3fae651d54633cf93ffc9c9b1b11cf
Maminirina Andriamasinoro <maminirinaandria17@gmail.com>
2025-03-14 20:05:35 +0300

    Update chemin ecran

--- gestion_ecran.py
+++ gestion_ecran.py
@@ -4,13 +4,12 @@ from kivy.uix.screenmanager import SlideTransition
 
 def gestion_ecran(root):
     root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/Home.kv'))
-    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/contrat.kv'))
-    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/planning.kv'))
+    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/historique/historique.kv'))
+    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/contrat/contrat.kv'))
+    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/planning/planning.kv'))
     root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/about.kv'))
-    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/Client.kv'))
-    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/planning.kv'))
-    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/historique.kv'))
-    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/compte.kv'))
-    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/compte_not_admin.kv'))
+    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/client/Client.kv'))
+    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/compte/compte.kv'))
+    root.get_screen('Sidebar').ids['gestion_ecran'].add_widget(Builder.load_file('screen/compte/compte_not_admin.kv'))
 
     root.get_screen('Sidebar').ids['gestion_ecran'].transition = SlideTransition(direction='up')
