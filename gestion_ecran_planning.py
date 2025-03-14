bfd4abdd390b8ada3e7fc6a89d18c4f7e61cd312
Maminirina Andriamasinoro <maminirinaandria17@gmail.com>
2025-03-14 20:01:48 +0300

    Creation de fichier planning et deplacement des ecrans

--- gestion_ecran_planning.py
+++ gestion_ecran_planning.py
@@ -2,4 +2,9 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 
 def gestion_ecran_planning(manager):
-    manager.add_widget(Builder.load_file(f'screen/rendu_planning.kv'))
+    manager.add_widget(Builder.load_file(f'screen/planning/rendu_planning.kv'))
+    manager.add_widget(Builder.load_file(f'screen/planning/option_decalage.kv'))
+    manager.add_widget(Builder.load_file(f'screen/planning/ecran_decalage.kv'))
+    manager.add_widget(Builder.load_file(f'screen/planning/selection_planning.kv'))
+    manager.add_widget(Builder.load_file(f'screen/planning/selection_tableau.kv'))
+    manager.add_widget(Builder.load_file(f'screen/planning/ajout_remarque.kv'))
